2cd46e8e9eefe7144e86eddfd6203d2cbf58ff4f
Francesco Furiani <francesco@furio.me>
2012-03-26 17:17:45 +0200

    domain cilindro

--- 20120326/eserciziDaCorreggere.js
+++ 20120326/eserciziDaCorreggere.js
@@ -47,3 +47,19 @@ var drawCircle = function(r,n) {
 	var mappedCircle = MAP(mappingCircle)(dominioCircle);
 	DRAW(mappedCircle);
 };
+
+/*****/
+
+var drawCilinder = function(r,h,n,m,color) {
+	var dominioCircle = DOMAIN([[0,2*PI],[0,h]])([n,m]);
+
+	var mappingCircle = function(p) {
+		var u = p[0];
+		var v = p[1];
+		return [r * COS(u), r * SIN(u), v];
+	};
+
+	var mappedCircle = MAP(mappingCircle)(dominioCircle);
+	DRAW(mappedCircle);
+	COLOR(color)(mappedCircle);
+};
