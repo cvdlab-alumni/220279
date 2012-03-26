04a817c7185610ff25f246a949d4738ee02c9f5d
Francesco Furiani <francesco@furio.me>
2012-03-26 16:07:20 +0200

    Commit iniziale per esercitazione 20120326

---
+++ appuntiPlasmBase.js
@@ -0,0 +1,12 @@
+/* CUBOID([dimX,dimY,dimZ]) */
+var c = CUBOID([1,1,1]);
+
+/* disegna */
+DRAW(c)
+
+/* 
+	Su tastiera premi c per centrare
+	Se esce il wireframe niente accelerazione grafica
+
+
+*/
