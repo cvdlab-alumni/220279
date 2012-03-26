f6c11bff396a87f0f7efaa7c5452059380ec97d3
Francesco Furiani <francesco@furio.me>
2012-03-26 17:06:02 +0200

    domain cerchio

--- 20120326/eserciziDaCorreggere.js
+++ 20120326/eserciziDaCorreggere.js
@@ -32,3 +32,17 @@ var mappingSin = function(p) {
 
 var mappedSin = MAP(mappingSin)(dominioSin);
 DRAW(mappedSin);
+
+/****/
+
+
+var drawCircle = function(r,n) {
+	var dominioCircle = DOMAIN([[0,2*PI]])([n]);
+
+	var mappingCircle = function(p) {
+		var u = p[0];
+		return [r * COS(u), r * SIN(u)];
+	};
+
+	var mappedCircle = MAP(mappingCircle)(dominioCircle);
+};
