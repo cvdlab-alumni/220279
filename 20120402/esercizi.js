576008e51d2358add1302b2b5e28a5064870d753
Francesco Furiani <francesco@furio.me>
2012-04-02 16:21:04 +0200

    travi

--- 20120402/esercizi.js
+++ 20120402/esercizi.js
@@ -3,3 +3,13 @@ var pillars = SIMPLEX_GRID([
 	[0.15, -6*2.4, 0.15],			   // y
 	[1.5,3,3]						   // z
 ]);
+
+DRAW(pillars);
+
+var beams = SIMPLEX_GRID([
+	REPLICA(3)([0.15, -6*2.4, 0.15]),  // x (*3)
+	[0.15 + 6*2.4 + 0.15],			   // y
+	[-(1.5 + 3 + 3), 1.5]						   // z
+]);
+
+DRAW(beams);
